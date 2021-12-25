affb5348096a0d5b272f177c3094b7999893b6de
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-12-26 01:57:06 +0900

    ワンページレイアウトに

--- public/js/vivus.js
+++ public/js/vivus.js
@@ -13,17 +13,24 @@ new Vivus(
   }
 );
 
-new Vivus(
+var logo = new Vivus(
   "text",
   {
     start: "autostart",
     type: "delayed",
-    duration: 1000,
+    duration: 300,
     animTimingFunction: Vivus.EASE,
   },
   function (logo) {
+    var fill = document.getElementsByClassName("cls-1");
+    for (let i = 0; i < fill.length; i++) {
+      fill[i].style.fill = "#ffd05b";
+    }
     setTimeout(function () {
+      for (let i = 0; i < fill.length; i++) {
+        fill[i].style.fill = "none";
+      }
       logo.reset().play();
-    }, 3000);
+    }, 5000);
   }
 );
