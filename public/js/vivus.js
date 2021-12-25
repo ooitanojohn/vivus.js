25d257615d2fc0fb23fe215ce6022421a7fe4fc5
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-12-26 00:56:29 +0900

    CDNやめた

--- public/js/vivus.js
+++ public/js/vivus.js
@@ -12,3 +12,16 @@ new Vivus(
     }, 3000);
   }
 );
+
+var logo = new Vivus(
+  "text",
+  {
+    type: "delayed",
+    duration: 1000,
+    animTimingFunction: Vivus.EASE,
+    start: "autostart",
+  },
+  function (obj) {
+    logo.reset().play();
+  }
+);
